8c868a08a8e9350363750756a50a462044b88b51
Manh CaNo <manhbn30594@framgia.com>
2018-11-22 15:09:26 +0700

    Chap5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,22 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+  before_action :set_locale
 
+  def hello
+    render html: "hello, world!"
+  end
 
-	def hello
-		render html: "hello, world!"
-	end
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,11 +9,15 @@ Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # tell the I18n library where to find your translations
+    I18n.load_path += Dir[Rails.root.join("lib", "locale", "*.{rb,yml}")]
+    I18n.available_locales = [:en, :vi]
 
+    # set default locale to something other than :en
+    I18n.default_locale = :vi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
+end
 end
