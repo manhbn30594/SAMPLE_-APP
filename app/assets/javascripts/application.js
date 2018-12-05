64b26556824bd0f9fbd9697ffdae52c460467f86
ManhCaNo <manhbn30594@gmail.com>
2018-12-05 08:34:01 +0700

    chap8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//= require rails-ujs
+//= require jquery
+//= require bootstrap
 //= require activestorage
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery3
-//= require popper
-//= require bootstrap
